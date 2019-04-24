1f99821542d7bc156e6dc6fc4d62ae20cc5c5a9f
USER <USER@AMP-26>
2019-04-24 18:15:59 +0900

    Commit

---
+++ chap11-ex11/src/ListChangeEx.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class ListChangeEx extends JFrame{
+	private JTextField tf = new JTextField(10);
+	private Vector<String> v = new Vector<String>();
+	private JList<String> nameList = new JList<String>(v);
+	
+	public ListChangeEx() {
+		setTitle("리스트 변경 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		
+		c.add(new JLabel("이름 입력 후 <Enter> 키"));
+		c.add(tf);
+		
+		v.add("황기태");
+		v.add("이재문");
+		nameList.setVisibleRowCount(5); // 리스트가 보여주는 행의 수
+		nameList.setFixedCellWidth(100); // 리스트의 폭
+		c.add(new JScrollPane(nameList));
+		
+		setSize(300,300);
+		setVisible(true);
+		
+		// JTextField에 ActionLister 등록, <Enter> 키 처리
+		tf.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JTextField t = (JTextField)e.getSource();
+				v.add(t.getText());
+				t.setText("");
+				nameList.setListData(v);
+			}
+		});
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			new ListChangeEx();
+	}
+
+}
