9c5bebaf0bf0b623ccd6ec1c88241392529bafa5
USER <USER@AMP-27>
2019-05-16 11:01:03 +0900

    Commit

---
+++ Test/src/Math.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class AA{
+	private int a,b;
+	
+}
+
+public class Math {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("숫자를 입력하시오");
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		char key = scanner.next().charAt(0);
+		switch (key) {
+		case '+':
+			
+			break;
+
+		default:
+			break;
+		}
+	}
+
+}
